c0ff1e8d82f8f5f58ce516308f57c8e03ff31f26
Joseph Elalam <joseph.elalam@optile.net>
2020-01-30 11:33:05 +0100

    update readme and render global error with the message but hidden

--- src/components/GlobalError/index.js
+++ src/components/GlobalError/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+// By default the global error component will be rendered,
+// but will be hidden and the message will not be displayed,
+// if needed, restyle .global-error by forcing display and changing colors and position
+const errorStyle = { display: "none" };
 /**
  * Global error that display a message
  * Display Global error message
@@ -9,7 +12,7 @@ import PropTypes from "prop-types";
  * @return {JSX.Element}
  */
 const GlobalError = ({ message }) => {
-    return <div test-id="global-error" className="global-error"></div>;
+    return <div test-id="global-error" className="global-error" style={errorStyle}>{message}</div>;
 };
 
 GlobalError.propTypes = {
