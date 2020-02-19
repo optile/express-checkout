cb59c925c26e50160c1e16daf748f24e7f5d7d21
Vanderlei Alves da Silva <vanderlei.alves.da.silva@gmail.com>
2020-02-19 12:16:43 +0100

    PCX-667: remove usuless demo code

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import { Products, Summary, Thankyou } from "./pages";
 
-import ExpressCheckout from "../../src";
 import "./style.css";
 
 const getMode = () => {
@@ -21,31 +20,6 @@ const Demo = () => {
         default:
             return <Products></Products>;
     }
-    return mode === "Successful" ? (
-        <div>Thank you for your payment!</div>
-    ) : (
-        <div>
-            {mode === "Cancel" && <div>An Error happened</div>}
-            {mode !== "Summary" && (
-                <ul className="products">
-                    <li>
-                        <strong>USB C cable: 2Euro</strong>
-                    </li>
-                </ul>
-            )}
-            <ExpressCheckout {...attributes} mode={mode} longId={longId} />
-            {mode !== "Summary" && (
-                <div>
-                    <ul className="products">
-                        <li>
-                            <strong>USB C cable fast: 10Euro</strong>
-                        </li>
-                    </ul>
-                    <ExpressCheckout {...attributes2} mode={mode} longId={longId} />
-                </div>
-            )}
-        </div>
-    );
 };
 
 render(<Demo />, document.querySelector("#demo"));
