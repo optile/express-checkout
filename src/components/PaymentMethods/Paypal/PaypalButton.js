f603b64e618b408d0d66e50eed3732ffe22f6519
dixy-xavier <dixy.xavier@optile.net>
2020-07-07 18:03:14 +0530

    PCX-791 handles wndow is undefined and undefined values in dispatch actions

--- src/components/PaymentMethods/Paypal/PaypalButton.js
+++ src/components/PaymentMethods/Paypal/PaypalButton.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import paypal from 'paypal-checkout';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // To prevent multiple call for paypal-checkout, because it causes issues
-if (!window.paypal) {
-  window.paypal = paypal;
+if (typeof window !== "undefined" && !window.paypal) {
+    const paypal = require("paypal-checkout");
+    window.paypal = paypal;
 }
 
-export default window.paypal.Button.driver('react', { React, ReactDOM });
+const ErrorHandling = () => <div>Server side rendering</div>;
+
+const PaypalButton = typeof window !== "undefined" ? window?.paypal?.Button?.driver("react", { React, ReactDOM }) : ErrorHandling;
+
+export default PaypalButton;
