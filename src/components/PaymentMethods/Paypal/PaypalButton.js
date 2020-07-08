2db0009dbfd56f2459787df8f254f30bd1c5dee3
dixy-xavier <dixy.xavier@optile.net>
2020-07-08 17:41:14 +0530

    PCX-791 removes redundant code

--- src/components/PaymentMethods/Paypal/PaypalButton.js
+++ src/components/PaymentMethods/Paypal/PaypalButton.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // To prevent multiple call for paypal-checkout, because it causes issues
-if (typeof window !== "undefined" && !window.paypal) {
+const isWindowDefined = typeof window !== "undefined";
+if (isWindowDefined && !window.paypal) {
     const paypal = require("paypal-checkout");
     window.paypal = paypal;
 }
 
 const Loading = () => <div>Please wait...</div>;
 
-const PaypalButton = typeof window !== "undefined" ? window?.paypal?.Button?.driver("react", { React, ReactDOM }) : Loading;
+const PaypalButton = isWindowDefined ? window?.paypal?.Button?.driver("react", { React, ReactDOM }) : Loading;
 
 export default PaypalButton;
