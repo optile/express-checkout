f4bbb7d472ee3318647df5ad8156f66b23d4dbef
Pritam Patil <pritam.patil@optile.net>
2020-08-10 17:51:41 +0200

    removed unused functions

--- src/functional-test/services/pageUtils.js
+++ src/functional-test/services/pageUtils.js
@@ -1,10 +1,5 @@
 const { Builder, By, until } = require('selenium-webdriver');
 
-// const isDocStateComplete = async () => {
-//     let readyState = await DRIVER.executeScript('return document.readyState');
-//     return readyState.toString() === 'complete';
-// };
-
 const checkUrlTitle = async title => {
     let currentUrl = await DRIVER.getCurrentUrl();
     return currentUrl.includes(title);
@@ -41,24 +36,6 @@ async function maximizeWindow() {
 async function switchToNextWindow() {
     return DRIVER.getAllWindowHandles().then(handles => switchToWindow(handles.pop()));
 }
-
-// async function refreshPage() {
-//     let navigator = await DRIVER.navigate();
-//     await navigator.refresh();
-//     return DRIVER.wait(() => isDocStateComplete());
-// }
-
-// function switchToFrame(index) {
-//     return DRIVER.wait(until.ableToSwitchToFrame(index), TIME);
-// }
-
-// function switchToDefaultContent() {
-//     return DRIVER.switchTo().defaultContent();
-// }
-
-// function switchToParentFrame() {
-//     return DRIVER.switchTo().parentFrame();
-// }
 
 async function waitForWindowCount(count) {
     return DRIVER.wait(() => checkWindowCount(count));
