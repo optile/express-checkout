5851927360729ebc9a7408c609e34b46f344c259
dixy-xavier <dixyxavier@gmail.com>
2020-09-17 15:53:39 +0200

    PCX-1020 removes the code

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -2,16 +2,4 @@ import Products from "./Products";
 import Summary from "./Summary";
 import Thankyou from "./Thankyou";
 
-window.addEventListener("message", event => {
-    if (JSON.parse(event?.data)?.__postRobot__?.data?.name === "onError") {
-        const interval = setInterval(() => {
-            const element = document.getElementById("react-refresh-overlay");
-            if (element) {
-                clearInterval(interval);
-                element.remove();
-            }
-        }, 0);
-    }
-});
-
 export { Products, Summary, Thankyou };
